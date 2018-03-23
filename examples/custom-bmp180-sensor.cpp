49d68d46071bd235d125b56dfdd1f76adf38e608
Otto Winter <otto@otto-winter.com>
2018-03-23 21:53:27 +0100

    Add custom sensor example

---
+++ examples/custom-bmp180-sensor.cpp
@@ -0,0 +1,53 @@
+//
+//  custom_sensor.cpp
+//  esphomelib
+//
+//  Created by Otto Winter on 23.03.18.
+//  Copyright © 2018 Otto Winter. All rights reserved.
+//
+
+// <WARNING>
+//   For this example to compile, you'll first need to install the Adafruit BMP085 library
+//   See  for more information
+// </WARNING>
+
+#include "esphomelib/application.h"
+#include <Adafruit_BMP085.h>
+
+using namespace esphomelib;
+
+class BMP180Sensor : public sensor::PollingSensorComponent {
+ public:
+  Adafruit_BMP085 bmp;
+
+  BMP180Sensor(uint32_t update_interval) : sensor::PollingSensorComponent(update_interval) {}
+
+  void setup() override {
+    bmp.begin();
+  }
+
+  void update() override {
+    int pressure = bmp.readPressure(); // in Pa, or 1/100 hPa
+    push_new_value(pressure / 100.0, 2); // convert to hPa, 2 decimal places of accuracy
+  }
+
+  std::string unit_of_measurement() override { return "hPa"; }
+};
+
+void setup() {
+  App.set_name("test");
+  App.init_log();
+
+  App.init_wifi("FRITZ!Box Fon WLAN 7360", "40678084986052753363");
+  App.init_ota()->start_safe_mode();
+  App.init_mqtt("MQTT_HOST", "USERNAME", "PASSWORD");
+
+  auto *custom_sensor = App.register_component(new BMP180Sensor(5000));
+  App.make_mqtt_sensor_for(custom_sensor, "Custom Sensor Example");
+
+  App.setup();
+}
+
+void loop() {
+  App.loop();
+}
