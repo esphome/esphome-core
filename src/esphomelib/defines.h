4d760abb28887d051dd7fb7e4c0469da9365a5db
Otto Winter <otto@otto-winter.com>
2018-06-01 18:48:36 +0200

    Bump version to 1.6.0

--- src/esphomelib/defines.h
+++ src/esphomelib/defines.h
@@ -9,7 +9,7 @@
 #ifndef ESPHOMELIB_DEFINES_H
 #define ESPHOMELIB_DEFINES_H
 
-#define ESPHOMELIB_VERSION "1.5.3"
+#define ESPHOMELIB_VERSION "1.6.0"
 
 #ifndef DOXYGEN
   #define ESPHOMELIB_NAMESPACE_BEGIN namespace esphomelib {
