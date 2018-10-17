bd40c4de9408af26c0a01738d743344d39650b44
Lazar Obradovic <laz.obradovic@gmail.com>
2018-10-17 20:46:46 +0200

    Fix PCF8574 assert. (#223)
    * Fix PCF8574 assert.
    
    * typo

--- examples/pcf8574/pcf8574.cpp
+++ examples/pcf8574/pcf8574.cpp
@@ -14,10 +14,13 @@ void setup() {
 
   auto *pcf8574 = App.make_pcf8574_component(0x21);
   App.make_gpio_binary_sensor("PCF pin 0 sensor", pcf8574->make_input_pin(0, PCF8574_INPUT));
+  App.make_gpio_binary_sensor("PCF pin 1 sensor", pcf8574->make_input_pin(1, PCF8574_INPUT_PULLUP));
+
   App.make_gpio_binary_sensor("PCF pin 0 sensor", 0);
-  App.make_gpio_switch("PCF pin 1 switch", pcf8574->make_output_pin(1));
-  auto *out = App.make_gpio_output(pcf8574->make_output_pin(2));
-  App.make_binary_light("PCF pin 2 light", out);
+
+  App.make_gpio_switch("PCF pin 4 switch", pcf8574->make_output_pin(4));
+  auto *out = App.make_gpio_output(pcf8574->make_output_pin(5));
+  App.make_binary_light("PCF pin 5 light", out);
 
   App.setup();
 }
