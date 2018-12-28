cb2a21feda2e25b886c0562e4d98edbdcfa8969a
voicevon <voicevon@gmail.com>
2018-12-29 05:52:56 +0800

    also esp8266

--- src/esphomelib/sensor/pulse_counter.cpp
+++ src/esphomelib/sensor/pulse_counter.cpp
@@ -77,9 +77,10 @@ bool PulseCounterBase::pulse_counter_setup_() {
 }
 pulse_counter_t PulseCounterBase::read_raw_value_() {
   pulse_counter_t counter = this->counter_;
-  pulse_counter_t ret = counter - this->last_value_;
-  this->last_value_ = counter;
-  return ret;
+  // pulse_counter_t ret = counter - this->last_value_;
+  // this->last_value_ = counter;
+  // return ret;
+  return counter;
 }
 #endif
 
