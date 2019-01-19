c20a31c82a8c44d2c1c04b6f22262c336ea07367
Otto Winter <otto@otto-winter.com>
2019-01-19 21:41:55 +0100

    Fix on_press / on_release being triggered on initial state (#425)

--- src/esphomelib/binary_sensor/filter.cpp
+++ src/esphomelib/binary_sensor/filter.cpp
@@ -8,31 +8,31 @@ ESPHOMELIB_NAMESPACE_BEGIN
 
 namespace binary_sensor {
 
-void Filter::output(bool value) {
+void Filter::output(bool value, bool is_initial) {
   if (this->next_ == nullptr) {
-    this->parent_->send_state_internal_(value);
+    this->parent_->send_state_internal_(value, is_initial);
   } else {
-    this->next_->input(value);
+    this->next_->input(value, is_initial);
   }
 }
-void Filter::input(bool value) {
-  auto b = this->new_value(value);
+void Filter::input(bool value, bool is_initial) {
+  auto b = this->new_value(value, is_initial);
   if (b.has_value()) {
-    this->output(*b);
+    this->output(*b, is_initial);
   }
 }
 DelayedOnFilter::DelayedOnFilter(uint32_t delay) : delay_(delay) {
 
 }
-optional<bool> DelayedOnFilter::new_value(bool value) {
+optional<bool> DelayedOnFilter::new_value(bool value, bool is_initial) {
   if (value) {
-    this->set_timeout("ON", this->delay_, [this](){
+    this->set_timeout("ON", this->delay_, [this, is_initial](){
       if (this->last_out_.value_or(false)) {
         // already sent ON
         return;
       }
       this->last_out_ = true;
-      this->output(true);
+      this->output(true, is_initial);
     });
     return {};
   } else {
@@ -53,15 +53,14 @@ float DelayedOnFilter::get_setup_priority() const {
 DelayedOffFilter::DelayedOffFilter(uint32_t delay) : delay_(delay) {
 
 }
-optional<bool> DelayedOffFilter::new_value(bool value) {
+optional<bool> DelayedOffFilter::new_value(bool value, bool is_initial) {
   if (!value) {
-    this->set_timeout("OFF", this->delay_, [this](){
+    this->set_timeout("OFF", this->delay_, [this, is_initial](){
       if (!this->last_out_.value_or(true)) {
         // already sent OFF
         return;
       }
-      this->last_out_ = false;
-      this->output(false);
+      this->output(false, is_initial);
     });
     return {};
   } else {
@@ -78,17 +77,17 @@ float DelayedOffFilter::get_setup_priority() const {
   return setup_priority::HARDWARE;
 }
 
-optional<bool> InvertFilter::new_value(bool value) {
+optional<bool> InvertFilter::new_value(bool value, bool is_initial) {
   return !value;
 }
 
 LambdaFilter::LambdaFilter(const std::function<optional<bool>(bool)> &f) : f_(f) {}
 
-optional<bool> LambdaFilter::new_value(bool value) {
+optional<bool> LambdaFilter::new_value(bool value, bool is_initial) {
   return this->f_(value);
 }
 
-optional<bool> UniqueFilter::new_value(bool value) {
+optional<bool> UniqueFilter::new_value(bool value, bool is_initial) {
   if (this->last_value_.has_value() && *this->last_value_ == value) {
     return {};
   } else {
@@ -98,14 +97,14 @@ optional<bool> UniqueFilter::new_value(bool value) {
 }
 
 HeartbeatFilter::HeartbeatFilter(uint32_t interval) : interval_(interval) {}
-optional<bool> HeartbeatFilter::new_value(bool value) {
+optional<bool> HeartbeatFilter::new_value(bool value, bool is_initial) {
   this->value_ = value;
   return value;
 }
 void HeartbeatFilter::setup() {
   this->set_interval(this->interval_, [this]() {
     if (this->value_.has_value())
-      this->output(*this->value_);
+      this->output(*this->value_, false);
   });
 }
 float HeartbeatFilter::get_setup_priority() const {
