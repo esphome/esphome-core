e6b1f504a3807ce1132c88e6eeb864a4571dca21
Otto Winter <otto@otto-winter.com>
2019-01-29 16:50:23 +0100

    Add cover

--- src/esphomelib/cover/cover.h
+++ src/esphomelib/cover/cover.h
@@ -30,6 +30,8 @@ template<typename T>
 class CloseAction;
 template<typename T>
 class StopAction;
+template<typename T>
+class CoverPublishAction;
 
 #define LOG_COVER(prefix, type, obj) \
     if (obj != nullptr) { \
@@ -57,6 +59,8 @@ class Cover : public Nameable {
   CloseAction<T> *make_close_action();
   template<typename T>
   StopAction<T> *make_stop_action();
+  template<typename T>
+  CoverPublishAction<T> *make_cover_publish_action();
 
   /** Return whether this cover is optimistic - i.e. if both the OPEN/CLOSE actions should be displayed in
    * Home Assistant because the real state is unknown.
@@ -111,6 +115,18 @@ class StopAction : public Action<T> {
   Cover *cover_;
 };
 
+template<typename T>
+class CoverPublishAction : public Action<T> {
+ public:
+  CoverPublishAction(Cover *cover);
+  void set_state(std::function<CoverState(T)> &&value);
+  void set_state(CoverState value);
+  void play(T x) override;
+ protected:
+  Cover *cover_;
+  TemplatableValue<CoverState, T> state_;
+};
+
 // =============== TEMPLATE DEFINITIONS ===============
 
 template<typename T>
@@ -160,6 +176,27 @@ template<typename T>
 StopAction<T> *Cover::make_stop_action() {
   return new StopAction<T>(this);
 }
+
+template<typename T>
+CoverSensorPublishAction<T>::CoverSensorPublishAction(Cover *cover) : cover_(cover) {}
+template<typename T>
+void CoverPublishAction<T>::set_state(std::function<CoverState(T)> &&value) {
+  this->state_ = std::move(value);
+}
+template<typename T>
+void CoverPublishAction<T>::set_state(CoverState value) {
+  this->state_ = value;
+}
+template<typename T>
+void CoverPublishAction<T>::play(T x) {
+  auto val = this->state_.value(x);
+  this->cover_->publish_state(*val);
+  this->play_next(x);
+}
+template<typename T>
+CoverPublishAction<T> *Cover::make_cover_publish_action() {
+  return new CoverPublishAction<T>(this);
+
 
 } // namespace cover
 
