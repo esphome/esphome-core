2acb431cd5615858082c3be8cf7aaf2386af69e5
mvturnho <qris.online@gmail.com>
2019-02-22 10:27:50 +0100

    add max channels to mpr121

--- src/esphome/application.cpp
+++ src/esphome/application.cpp
@@ -1271,8 +1271,8 @@ sensor::APDS9960 *Application::make_apds9960(uint32_t update_interval) {
 #endif
 
 #ifdef USE_MPR121
-sensor::MPR121_Sensor *Application::make_mpr121(uint8_t address) {
-  return this->register_component(new MPR121_Sensor(this->i2c_, address));
+sensor::MPR121_Sensor *Application::make_mpr121(uint8_t address, uint8_t channels) {
+  return this->register_component(new MPR121_Sensor(this->i2c_, address, channels));
 }
 #endif
 
