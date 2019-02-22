c6dcffa8044d61cae9bdaed4a218c2dcf3cd9c34
mvturnho <qris.online@gmail.com>
2019-02-22 18:20:48 +0100

    fixed issues from pull request

--- src/esphome/sensor/mpr121_sensor.cpp
+++ src/esphome/sensor/mpr121_sensor.cpp
@@ -1,5 +1,4 @@
 #include "esphome/defines.h"
-#define USE_MPR121 1
 #ifdef USE_MPR121
 
 #include "esphome.h"
@@ -10,116 +9,114 @@ ESPHOME_NAMESPACE_BEGIN
 
 namespace sensor {
 
-    static const char *TAG = "sensor.mpr121";
+static const char *TAG = "sensor.mpr121";
 
-    MPR121_Channel::MPR121_Channel(const std::string &name,int channel_num) : BinarySensor(name) {
-      //ESP_LOGD(TAG, "channel %d " , channel_num);
-      channel = channel_num;
-    }
+MPR121_Channel::MPR121_Channel(const std::string &name, int channel_num) : BinarySensor(name) {
+  channel_ = channel_num;
+}
 
-    void MPR121_Channel::process_(uint16_t *data, uint16_t *last_data) {
-        if ((*data & _BV(channel)) && !(*last_data & _BV(channel))) {
-            this->publish_state(true);
-        }
-        if (!(*data & _BV(channel)) && (*last_data & _BV(channel)) ) {
-            this->publish_state(false);
-        }
-    }
+void MPR121_Channel::process(uint16_t *data, uint16_t *last_data) {
+  if ((*data & _BV(channel_)) && !(*last_data & _BV(channel_))) {
+    this->publish_state(true);
+  }
+  if (!(*data & _BV(channel_)) && (*last_data & _BV(channel_))) {
+    this->publish_state(false);
+  }
+}
 
-    MPR121_Sensor::MPR121_Sensor(I2CComponent *parent, uint8_t address) :  I2CDevice(parent, address) {}
+MPR121_Sensor::MPR121_Sensor(I2CComponent *parent, uint8_t address) : I2CDevice(parent, address) {
+}
 
-    void MPR121_Sensor::setup() {
-        ESP_LOGCONFIG(TAG, "Setting up MPR121...");
-        // soft reset
-        if (!this->write_byte(MPR121_SOFTRESET, 0x63)) {
-            this->error_code_ = COMMUNICATION_FAILED;
-            this->mark_failed();
-            return;
-        }
-        delay(1);
-        this->write_byte(MPR121_ECR, 0x0);
+void MPR121_Sensor::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up MPR121...");
+  // soft reset
+  if (!this->write_byte(MPR121_SOFTRESET, 0x63)) {
+    this->error_code_ = COMMUNICATION_FAILED;
+    this->mark_failed();
+    return;
+  }
+  delay(1);
+  this->write_byte(MPR121_ECR, 0x0);
 
-        //set touch sensitivity
-        //this->set_sensitivity();
-        for (uint8_t i=0; i<12; i++) {
-            this->write_byte(MPR121_TOUCHTH_0 + 2*i, 12);
-            this->write_byte(MPR121_RELEASETH_0 + 2*i, 6);
-        }
-        this->write_byte(MPR121_MHDR, 0x01);
-        this->write_byte(MPR121_NHDR, 0x01);
-        this->write_byte(MPR121_NCLR, 0x0E);
-        this->write_byte(MPR121_FDLR, 0x00);
+  // set touch sensitivity
+  // this->set_sensitivity();
+  for (uint8_t i = 0; i < 12; i++) {
+    this->write_byte(MPR121_TOUCHTH_0 + 2 * i, 12);
+    this->write_byte(MPR121_RELEASETH_0 + 2 * i, 6);
+  }
+  this->write_byte(MPR121_MHDR, 0x01);
+  this->write_byte(MPR121_NHDR, 0x01);
+  this->write_byte(MPR121_NCLR, 0x0E);
+  this->write_byte(MPR121_FDLR, 0x00);
 
-        this->write_byte(MPR121_MHDF, 0x01);
-        this->write_byte(MPR121_NHDF, 0x05);
-        this->write_byte(MPR121_NCLF, 0x01);
-        this->write_byte(MPR121_FDLF, 0x00);
+  this->write_byte(MPR121_MHDF, 0x01);
+  this->write_byte(MPR121_NHDF, 0x05);
+  this->write_byte(MPR121_NCLF, 0x01);
+  this->write_byte(MPR121_FDLF, 0x00);
 
-        this->write_byte(MPR121_NHDT, 0x00);
-        this->write_byte(MPR121_NCLT, 0x00);
-        this->write_byte(MPR121_FDLT, 0x00);
+  this->write_byte(MPR121_NHDT, 0x00);
+  this->write_byte(MPR121_NCLT, 0x00);
+  this->write_byte(MPR121_FDLT, 0x00);
 
-        this->write_byte(MPR121_DEBOUNCE, 0);
-        this->write_byte(MPR121_CONFIG1, 0x10); // default, 16uA charge current
-        this->write_byte(MPR121_CONFIG2, 0x20); // 0.5uS encoding, 1ms period
+  this->write_byte(MPR121_DEBOUNCE, 0);
+  this->write_byte(MPR121_CONFIG1, 0x10);  // default, 16uA charge current
+  this->write_byte(MPR121_CONFIG2, 0x20);  // 0.5uS encoding, 1ms period
 
-        this->write_byte(MPR121_ECR, 0x8F);     // start with first 5 bits of baseline tracking
+  this->write_byte(MPR121_ECR, 0x8F);  // start with first 5 bits of baseline tracking
+}
 
-    }
+void MPR121_Sensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "MPR121:");
+  LOG_I2C_DEVICE(this);
+  switch (this->error_code_) {
+    case COMMUNICATION_FAILED:
+      ESP_LOGE(TAG, "Communication with MPR121 failed!");
+      break;
+    case WRONG_CHIP_STATE:
+      ESP_LOGE(TAG, "MPR121 has wrong default value for CONFIG2?");
+      break;
+    case NONE:
+    default:
+      break;
+  }
+}
 
-    void MPR121_Sensor::dump_config() {
-        ESP_LOGCONFIG(TAG, "MPR121:");
-        LOG_I2C_DEVICE(this);
-        switch (this->error_code_) {
-            case COMMUNICATION_FAILED:
-                ESP_LOGE(TAG, "Communication with MPR121 failed!");
-                break;
-            case WRONG_CHIP_STATE:
-                ESP_LOGE(TAG, "MPR121 has wrong default value for CONFIG2?");
-                break;
-            case NONE:
-            default:
-                break;
-        }
-        ESP_LOGCONFIG(TAG, "  currvalue: %d", this->read_mpr121_channels());
-        //LOG_UPDATE_INTERVAL(this);
-    }
+float MPR121_Sensor::get_setup_priority() const {
+  return setup_priority::HARDWARE_LATE;
+}
 
-    MPR121_Channel *MPR121_Sensor::add_channel(sensor::MPR121_Channel* channel) {
-      this->channels.push_back(channel);
-      return channel;
-    }
+MPR121_Channel *MPR121_Sensor::add_channel(sensor::MPR121_Channel *channel) {
+  this->channels_.push_back(channel);
+  return channel;
+}
 
-    void MPR121_Sensor::process_( uint16_t *data, uint16_t *last_data) {
-      for (auto *channel : this->channels) {
-        channel->process_(data, last_data);
-      }
-    }
+void MPR121_Sensor::process_(uint16_t *data, uint16_t *last_data) {
+  for (auto *channel : this->channels_) {
+    channel->process(data, last_data);
+  }
+}
 
-    uint16_t MPR121_Sensor::read_mpr121_channels() {
-        uint16_t val = 0;
-        this->read_byte_16(MPR121_TOUCHSTATUS_L, &val);
-        uint8_t lsb = val >> 8;
-        uint8_t msb = val ;
-        val = ((uint16_t) msb) << 8;
-        val |= lsb;
+uint16_t MPR121_Sensor::read_mpr121_channels_() {
+  uint16_t val = 0;
+  this->read_byte_16(MPR121_TOUCHSTATUS_L, &val);
+  uint8_t lsb = val >> 8;
+  uint8_t msb = val;
+  val = ((uint16_t)msb) << 8;
+  val |= lsb;
+  return val;
+}
 
-        //ESP_LOGD(TAG,"msb:%d lsb:%d value: %d",msb,lsb,val);
-        return val;
-    }
+void MPR121_Sensor::loop() {
+  this->currtouched_ = this->read_mpr121_channels_();
+  if (this->currtouched_ != this->lasttouched_) {
+    this->process_(&currtouched_, &lasttouched_);
+  }
+  // reset touchstate
+  this->lasttouched_ = this->currtouched_;
+}
 
-    void MPR121_Sensor::loop() {
-        this->currtouched = this->read_mpr121_channels();
-        if(this->currtouched != this->lasttouched) {  
-            this->process_(&currtouched,&lasttouched);
-        }
-        // reset touchstate
-        this->lasttouched = this->currtouched;
-    }
-
-
- } // namespace sensor
+}  // namespace sensor
 
 ESPHOME_NAMESPACE_END
 
-#endif //USE_BH1750
+#endif  // USE_BH1750
--- src/esphome/sensor/mpr121_sensor.h
+++ src/esphome/sensor/mpr121_sensor.h
@@ -10,88 +10,86 @@
 #include "esphome/i2c_component.h"
 #include "esphome/component.h"
 
-static const uint8_t MPR121_DEFAULT_I2C_ADDR = 0x5A;
-
 ESPHOME_NAMESPACE_BEGIN
 
 namespace sensor {
 
-    enum {
-        MPR121_TOUCHSTATUS_L = 0x00,
-        MPR121_TOUCHSTATUS_H = 0x01,
-        MPR121_FILTDATA_0L  = 0x04,
-        MPR121_FILTDATA_0H  = 0x05,
-        MPR121_BASELINE_0   = 0x1E,
-        MPR121_MHDR         = 0x2B,
-        MPR121_NHDR         = 0x2C,
-        MPR121_NCLR         = 0x2D,
-        MPR121_FDLR         = 0x2E,
-        MPR121_MHDF         = 0x2F,
-        MPR121_NHDF         = 0x30,
-        MPR121_NCLF         = 0x31,
-        MPR121_FDLF         = 0x32,
-        MPR121_NHDT         = 0x33,
-        MPR121_NCLT         = 0x34,
-        MPR121_FDLT         = 0x35,
+enum {
+  MPR121_TOUCHSTATUS_L = 0x00,
+  MPR121_TOUCHSTATUS_H = 0x01,
+  MPR121_FILTDATA_0L = 0x04,
+  MPR121_FILTDATA_0H = 0x05,
+  MPR121_BASELINE_0 = 0x1E,
+  MPR121_MHDR = 0x2B,
+  MPR121_NHDR = 0x2C,
+  MPR121_NCLR = 0x2D,
+  MPR121_FDLR = 0x2E,
+  MPR121_MHDF = 0x2F,
+  MPR121_NHDF = 0x30,
+  MPR121_NCLF = 0x31,
+  MPR121_FDLF = 0x32,
+  MPR121_NHDT = 0x33,
+  MPR121_NCLT = 0x34,
+  MPR121_FDLT = 0x35,
 
-        MPR121_TOUCHTH_0    = 0x41,
-        MPR121_RELEASETH_0  = 0x42,
-        MPR121_DEBOUNCE     = 0x5B,
-        MPR121_CONFIG1      = 0x5C,
-        MPR121_CONFIG2      = 0x5D,
-        MPR121_CHARGECURR_0 = 0x5F,
-        MPR121_CHARGETIME_1 = 0x6C,
-        MPR121_ECR          = 0x5E,
-        MPR121_AUTOCONFIG0  = 0x7B,
-        MPR121_AUTOCONFIG1  = 0x7C,
-        MPR121_UPLIMIT      = 0x7D,
-        MPR121_LOWLIMIT     = 0x7E,
-        MPR121_TARGETLIMIT  = 0x7F,
+  MPR121_TOUCHTH_0 = 0x41,
+  MPR121_RELEASETH_0 = 0x42,
+  MPR121_DEBOUNCE = 0x5B,
+  MPR121_CONFIG1 = 0x5C,
+  MPR121_CONFIG2 = 0x5D,
+  MPR121_CHARGECURR_0 = 0x5F,
+  MPR121_CHARGETIME_1 = 0x6C,
+  MPR121_ECR = 0x5E,
+  MPR121_AUTOCONFIG0 = 0x7B,
+  MPR121_AUTOCONFIG1 = 0x7C,
+  MPR121_UPLIMIT = 0x7D,
+  MPR121_LOWLIMIT = 0x7E,
+  MPR121_TARGETLIMIT = 0x7F,
 
-        MPR121_GPIODIR      = 0x76,
-        MPR121_GPIOEN       = 0x77,
-        MPR121_GPIOSET      = 0x78,
-        MPR121_GPIOCLR      = 0x79,
-        MPR121_GPIOTOGGLE   = 0x7A,
+  MPR121_GPIODIR = 0x76,
+  MPR121_GPIOEN = 0x77,
+  MPR121_GPIOSET = 0x78,
+  MPR121_GPIOCLR = 0x79,
+  MPR121_GPIOTOGGLE = 0x7A,
 
-        MPR121_SOFTRESET    = 0x80,
-    };
+  MPR121_SOFTRESET = 0x80,
+};
 
-    class MPR121_Channel : public binary_sensor::BinarySensor {
-        public:
-            int channel = 0;
+class MPR121_Channel : public binary_sensor::BinarySensor {
+ public:
+  MPR121_Channel(const std::string &name, int channel_num = 0);
+  void process(uint16_t *data, uint16_t *last_data);
 
-            MPR121_Channel(const std::string &name,int channel_num = 0);
-            void process_(uint16_t *data, uint16_t *last_data);
-    };
+ protected:
+  int channel_ = 0;
+};
 
-    class MPR121_Sensor : public Component , public I2CDevice {
-        public:
-            MPR121_Sensor(I2CComponent *parent, uint8_t address = MPR121_I2CADDR_DEFAULT);
-            sensor::MPR121_Channel *add_channel(sensor::MPR121_Channel *channel);
-            void process_(uint16_t *data,uint16_t *last_data);
-            void setup() override;
-            void dump_config() override;
-            //float get_setup_priority() const override;
-            void loop() override;
-        protected:
-            std::vector<MPR121_Channel *> channels{};
-            uint16_t lasttouched = 0;
-            uint16_t currtouched = 0;
-            enum ErrorCode {
-                NONE = 0,
-                COMMUNICATION_FAILED,
-                WRONG_CHIP_STATE,
-            } error_code_{NONE};
-            uint16_t read_mpr121_channels();
-    };
+class MPR121_Sensor : public Component, public I2CDevice {
+ public:
+  MPR121_Sensor(I2CComponent *parent, uint8_t address = 0x5A);
+  sensor::MPR121_Channel *add_channel(sensor::MPR121_Channel *channel);
+  void setup() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+  void loop() override;
 
+ protected:
+  std::vector<MPR121_Channel *> channels_{};
+  uint16_t lasttouched_ = 0;
+  uint16_t currtouched_ = 0;
+  enum ErrorCode {
+    NONE = 0,
+    COMMUNICATION_FAILED,
+    WRONG_CHIP_STATE,
+  } error_code_{NONE};
+  void process_(uint16_t *data, uint16_t *last_data);
+  uint16_t read_mpr121_channels_();
+};
 
-
-} // namespace sensor
+}  // namespace sensor
 
 ESPHOME_NAMESPACE_END
 
-#endif //USE_MPR121
+#endif  // USE_MPR121
 
-#endif //ESPHOME_SENSOR_MPR121_H
+#endif  // ESPHOME_SENSOR_MPR121_H
