9a5793b8eff77abc8c4a9ebe302bdfa3f726fbe8
mvturnho <qris.online@gmail.com>
2019-03-01 10:33:18 +0100

    calculation for SLIDER sensor type and code cleanup

--- src/esphome/sensor/mpr121_sensor.cpp
+++ src/esphome/sensor/mpr121_sensor.cpp
@@ -18,12 +18,31 @@ MPR121SensorChannel::MPR121SensorChannel(uint8_t channel, float value) {
 MPR121Sensor::MPR121Sensor(const std::string &name) : Sensor(name) { }
 
 void MPR121Sensor::process(uint16_t *data, uint16_t *last_data) {
-  //process should depend on the sensor_type_
+  //process depend on the sensor_type_
+  switch(this->sensor_type_){
+    case MPR121_SENSOR_TYPE_SLIDER: 
+      this->process_slider_(data);
+      break;
+    case MPR121_SENSOR_TYPE_ROTARY: 
+      ESP_LOGD(TAG, "'%s': Unimplemented sensor type %d", this->name_.c_str(), this->sensor_type_);
+      break;
+    default:
+      ESP_LOGE(TAG, "'%s': Unknown sensor type %d", this->name_.c_str(), this->sensor_type_);
+  }
+}
+
+void MPR121Sensor::process_slider_(uint16_t *data) {
+  uint8_t count = 0;
+  float value = 0;
   for(auto *chan : this->channels_) {
-    if ((*data & (1 << chan->channel)) && !(*last_data & (1 << chan->channel))) {
-      this->publish_state(chan->value);
-      ESP_LOGD(TAG,"process channel value %.2f",chan->value);
+    if ((*data & (1 << chan->channel)) ) {
+      value += chan->value;
+      count++;
     }
+  }
+  if(count > 0) {
+    this->publish_state(value/count);
+    ESP_LOGD(TAG, "'%s': Sending new state %f", this->name_.c_str(), state);
   }
 }
 
--- src/esphome/sensor/mpr121_sensor.h
+++ src/esphome/sensor/mpr121_sensor.h
@@ -11,11 +11,12 @@ ESPHOME_NAMESPACE_BEGIN
 
 namespace sensor {
 
-enum {
-  MPR121_SENSOR_TYPE_SLIDER = 0x00,
-  MPR121_SENSOR_TYPE_ROTARY = 0x01,
-};
 
+/** SensorChannel class just a placeholder for each mpr121 sensor channel.
+ *
+ * The channels together form one Sensor. 
+ * The Sensor value is calculated by the values of the channels that are active.
+ */
 class MPR121SensorChannel {
  public:
   MPR121SensorChannel(uint8_t channel_num = 0, float value = 0.0);
@@ -24,17 +25,28 @@ class MPR121SensorChannel {
   float value = 0.0;
 };
 
+/** Sensor class for all sensor-type functions of the mpr121.
+ *
+ * This class processes the touch action on the channels
+ * and publishes the calculated value for this sensor
+ * based on the sensortype SIDER or ROTARY
+ */
 class MPR121Sensor : public sensor::Sensor {
  public:
+  enum {
+    MPR121_SENSOR_TYPE_SLIDER = 0x00,
+    MPR121_SENSOR_TYPE_ROTARY = 0x01,
+  }; 
+
   MPR121Sensor(const std::string &name);
   void process(uint16_t *data, uint16_t *last_data);
   void set_sensor_type(uint8_t sensor_type);
   void add_sensor_channel(uint8_t channel, float value);
-  static const uint8_t MPR121_SENSOR_TYPE_SLIDER = 0;
 
  protected:
   std::vector<MPR121SensorChannel *> channels_{};
   uint8_t sensor_type_ = MPR121_SENSOR_TYPE_SLIDER;
+  void process_slider_(uint16_t *data);
 };
 
 }  // namespace binary_sensor
