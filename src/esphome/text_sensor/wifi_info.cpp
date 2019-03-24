d146d8592cabe8072c23d7f4b717bca0b9840af1
Otto Winter <otto@otto-winter.com>
2019-03-24 18:01:34 +0100

    Fix WiFi Info sensor for ESP32 (#561)
    * Fix WiFi Info sensor for ESP32
    
    Fixes https://github.com/esphome/issues/issues/155
    
    * Lint

--- src/esphome/text_sensor/wifi_info.cpp
+++ src/esphome/text_sensor/wifi_info.cpp
@@ -20,6 +20,7 @@ void IPAddressWiFiInfo::loop() {
   }
 }
 IPAddressWiFiInfo::IPAddressWiFiInfo(const std::string &name) : TextSensor(name) {}
+float IPAddressWiFiInfo::get_setup_priority() const { return setup_priority::WIFI - 1.0f; }
 
 void SSIDWiFiInfo::loop() {
   String ssid = WiFi.SSID();
@@ -29,6 +30,7 @@ void SSIDWiFiInfo::loop() {
   }
 }
 SSIDWiFiInfo::SSIDWiFiInfo(const std::string &name) : TextSensor(name) {}
+float SSIDWiFiInfo::get_setup_priority() const { return setup_priority::WIFI - 1.0f; }
 
 void BSSIDWiFiInfo::loop() {
   uint8_t *bssid = WiFi.BSSID();
@@ -40,6 +42,7 @@ void BSSIDWiFiInfo::loop() {
   }
 }
 BSSIDWiFiInfo::BSSIDWiFiInfo(const std::string &name) : TextSensor(name) {}
+float BSSIDWiFiInfo::get_setup_priority() const { return setup_priority::WIFI - 1.0f; }
 
 }  // namespace text_sensor
 
