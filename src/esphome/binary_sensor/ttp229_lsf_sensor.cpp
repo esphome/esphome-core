758074f2eee67fcbc6020e46952fe363f5cb6cb3
Otto Winter <otto@otto-winter.com>
2019-03-31 11:37:39 +0200

    Update ttp229_lsf_sensor.cpp
    - Simplify loop(); the method is so simple that it doesn't need any helper methods.
    - Add error handling
    - Simplify process(), no idea why pointers were used here

--- src/esphome/binary_sensor/ttp229_lsf_sensor.cpp
+++ src/esphome/binary_sensor/ttp229_lsf_sensor.cpp
@@ -11,9 +11,9 @@ namespace binary_sensor {
 
 static const char *TAG = "binary_sensor.ttp229";
 
-TTP229Channel::TTP229Channel(const std::string &name, int channel_num) : BinarySensor(name) { channel_ = channel_num; }
+TTP229Channel::TTP229Channel(const std::string &name, int channel_num) : BinarySensor(name), channel_(channel_num) { }
 
-void TTP229Channel::process(const uint16_t *data) { this->publish_state(*data & (1 << this->channel_)); }
+void TTP229Channel::process(uint16_t data) { this->publish_state(data & (1 << this->channel_)); }
 
 TTP229LSFComponent::TTP229LSFComponent(I2CComponent *parent, uint8_t address) : I2CDevice(parent, address) {}
 
@@ -31,7 +31,7 @@ void TTP229LSFComponent::dump_config() {
   LOG_I2C_DEVICE(this);
   switch (this->error_code_) {
     case COMMUNICATION_FAILED:
-      ESP_LOGE(TAG, "Communication with ttp229 failed!");
+      ESP_LOGE(TAG, "Communication with TTP229 failed!");
       break;
     case NONE:
     default:
@@ -46,25 +46,17 @@ TTP229Channel *TTP229LSFComponent::add_channel(binary_sensor::TTP229Channel *cha
   return channel;
 }
 
-void TTP229LSFComponent::process_(uint16_t *data) {
-  for (auto *channel : this->channels_) {
-    channel->process(data);
-  }
-}
-
-uint16_t TTP229LSFComponent::read_channels_() {
-  uint16_t val = 0;
-  this->parent_->raw_receive_16(this->address_, &val, 1);
-  return reverse_bits_16(val);
-}
-
 void TTP229LSFComponent::loop() {
-  this->currtouched_ = this->read_channels_();
-  if (this->currtouched_ != this->lasttouched_) {
-    this->process_(&currtouched_);
+  uint16_t touched = 0;
+  if (!this->parent_->raw_receive_16(this->address_, &touched, 1)) {
+    this->status_set_warning();
+    return;
   }
-  // reset touchsensor state
-  this->lasttouched_ = this->currtouched_;
+  this->status_clear_warning();
+  touched = reverse_bits_16(touched);
+  for (auto *channel : this->channels_) {
+    channel->process(touched);
+  }
 }
 
 }  // namespace binary_sensor
