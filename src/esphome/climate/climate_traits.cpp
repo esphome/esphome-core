517751050fc782ad8f73d205e4ab727d1a5e56e3
Otto Winter <otto@otto-winter.com>
2019-04-08 21:11:40 +0200

    Lint

--- src/esphome/climate/climate_traits.cpp
+++ src/esphome/climate/climate_traits.cpp
@@ -51,7 +51,7 @@ int8_t ClimateTraits::get_temperature_accuracy_decimals() const {
   char buf[32];
   sprintf(buf, "%.5g", this->visual_temperature_step_);
   std::string str{buf};
-  size_t dot_pos = str.find(".");
+  size_t dot_pos = str.find('.');
   if (dot_pos == std::string::npos)
     return 0;
 
